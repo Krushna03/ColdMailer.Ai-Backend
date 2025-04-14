b017d315dc5c4d555bb1b8a4d9e5dff8c20076f9
Krushna03 <krushnasakhare965@gmail.com>
2025-04-14 13:00:50 +0530

    Frontend url added

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import cors from "cors"
 const app = express()
 
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:5174'],
+  origin: [ process.env.CLIENT_URL, 'http://localhost:5173', 'http://localhost:5174'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS', 'HEAD'],
 }));
