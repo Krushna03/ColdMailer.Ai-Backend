3ea5570f31b29bb4e32686f47d30e02a6d402df3
Krushna03 <krushnasakhare965@gmail.com>
2025-05-05 01:09:54 +0530

    backend added for contact

--- src/index.js
+++ src/index.js
@@ -1,26 +1,10 @@
-import { GoogleGenerativeAI } from "@google/generative-ai";
 import { app } from './app.js'
 import dotenv from 'dotenv'
 import connectDB from './databse/db.js'
-import { OAuth2Client } from 'google-auth-library';
 
 dotenv.config({
   path: "./.env"
 })
-
-
-export const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
-
-const geminiapiKey = process.env.GEMINIAPIKEY;
-const genAI = new GoogleGenerativeAI(geminiapiKey);
-
-export const model = genAI.getGenerativeModel({
-  model: "gemini-2.0-flash",
-  generationConfig: {
-    maxOutputTokens: 1000,
-    temperature: 0.3,
-  }
-});
 
 
 app.get('/', (req, res) => {
